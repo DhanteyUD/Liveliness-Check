878ef2562850454d6907e47d1406ae2412974ff7
Clinton Otse <otseclinton60@gmail.com>
2025-02-23 19:43:35 +0100

    Facial Recognition Models

--- liveliness-check/src/App.jsx
+++ liveliness-check/src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import LivenessCheck from "./components/liveliness-check";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-[red] w-[300px] h-[300px] flex items-center justify-center">
+        {/* <LivenessCheck /> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
